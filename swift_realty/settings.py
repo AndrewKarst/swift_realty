93e223bf0d4cd9ae38dc4fc5232f705c180a1187
Mentat <karst14@gmail.com>
2020-11-24 20:40:57 -0500

    Settings

--- swift_realty/settings.py
+++ swift_realty/settings.py
@@ -165,6 +165,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'media'),)
